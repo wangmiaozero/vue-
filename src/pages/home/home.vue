c24f9362c1359c5908996bedaa96690d6e98a797
wangmiaozero <1061214467@qq.com>
2019-02-12 20:52:09 +0800

    九空格完成

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -9,24 +9,24 @@
             </mt-swipe-item>
         </mt-swipe>
         <!--九空格-->
-         <ul class="mui-table-view mui-grid-view mui-grid-9">
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
-		                   
+         <ul class="mui-table-view mui-grid-view mui-grid-9 my-ul">
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3 "><a href="#" class="my-box">
+                                <img src="@/assets/menu1.png" alt="">
 		                    <div class="mui-media-body">新闻资讯</div></a></li>
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
-		                  
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#" class="my-box">
+                                <img src="@/assets/menu2.png" alt="">
 		                    <div class="mui-media-body">图片分享</div></a></li>
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
-		                 
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#" class="my-box">
+                                <img src="@/assets/menu3.png" alt="">
 		                    <div class="mui-media-body">商品购买</div></a></li>
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
-		                  
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#" class="my-box">
+                                <img src="@/assets/menu4.png" alt="">
 		                    <div class="mui-media-body">留言反馈</div></a></li>
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
-		                 
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#" class="my-box">
+                                <img src="@/assets/menu5.png" alt="">
 		                    <div class="mui-media-body">视频专区</div></a></li>
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
-		                 
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#" class="my-box">
+                                <img src="@/assets/menu6.png" alt="">
 		                    <div class="mui-media-body">联系我们</div></a></li>
 		        </ul> 
     </div>
@@ -58,6 +58,15 @@ export default {
                     width: 100%;
                     height: 100%;
                 }
+            }
+        }
+        .my-ul{
+            background: #fff;
+        }
+        .my-box{
+            img{
+                width: 80%;
+                height: 80%;
             }
         }
     }
